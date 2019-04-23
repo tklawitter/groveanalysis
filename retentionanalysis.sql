f7e3caa59b6e738a69e250f3ef85db3e3f11946e
tklawitter <49925144+tklawitter@users.noreply.github.com>
2019-04-23 15:03:38 -0700

    Create retentionanalysis.sql

---
+++ retentionanalysis.sql
@@ -0,0 +1,6 @@
+SELECT order_number, 
+       Count(*)                    AS TOTAL_ORDERS, 
+       Avg(Substr(order_value, 2)) AS AVG_VALUE, 
+       Sum(Substr(order_value, 2)) AS TOTAL_VALUE 
+FROM   orders 
+GROUP  BY order_number; 
